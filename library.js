112c41667236217894a6bc2cd9835b878e572baa
Paul Alexander <myotheremail@oh.rr.com>
2019-11-13 19:04:48 -0500

    Add testing function and css

--- library.js
+++ library.js
@@ -1,9 +1,24 @@
+let myLibrary = [];
+const createBookDiv = function(e) {
+    const CONTAINER = document.getElementById('main-container');
+    const BDIV = document.createElement('DIV');
+    BDIV.className = 'container'
+    BDIV.textContent = 'Another One.';
+    CONTAINER.append(BDIV);
+}
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+    /*this.info = function() {
         return `${this.title}, ${this.pages} pages, ${this.read == true ? 'read' : 'not read yet'}`;
-    };
+    };*/
 }
+
+Book.prototype.info = function() {
+    return `${this.title}, ${this.pages} pages, ${this.read == true ? 'read' : 'not read yet'}`;
+};
+
+//document.querySelector('h1').addEventListener('click', createBookDiv)
